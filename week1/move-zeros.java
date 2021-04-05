b91c0ccf708e31c84a47dad90b49f066bc72ffe3
Xuezhong <Xuezhong.yan1@ge.com>
2021-04-05 16:40:34 +0800

    Add solution 2:  kind like quick sort idea.

--- week1/move-zeros.java
+++ week1/move-zeros.java
@@ -1,17 +1,31 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        if (nums == null) {
+            return;
+        }
+        // Solution 2, double pointers i and j, moving non-zero to left, zero to right, like quick sort
         int j = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                // move non-zero forward
-                nums[j++] = nums[i];
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j++] = tmp;
             }
         }
+
+        // // Solution 1, moving non-zero nums forward, then appending zero to tail
+        // int j = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] != 0) {
+        //         // move non-zero forward
+        //         nums[j++] = nums[i];
+        //     }
+        // }
         
-        // append zeros
-        for (int i = j; i < nums.length; i++) {
-            nums[i] = 0; 
-        }
+        // // append zeros
+        // for (int i = j; i < nums.length; i++) {
+        //     nums[i] = 0; 
+        // }
     }
 }
 
