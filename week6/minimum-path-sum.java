5adbf18f21f5a10d0eddd279205d91c512f9d912
Xuezhong <Xuezhong.yan1@ge.com>
2021-05-23 21:48:33 +0800

    Use an extra dp array.

--- week6/minimum-path-sum.java
+++ week6/minimum-path-sum.java
@@ -5,23 +5,27 @@ class Solution {
         }
         int row = grid.length;
         int col = grid[0].length;
-        // int[][]dp = new int[row][col];
-        // dp = grid;
+        int[][]dp = new int[row][col];
+        dp = grid;
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
                 if (i == 0 && j == 0) {
                     continue;
                 }
                 if (i == 0) { // top border
-                    grid[i][j] = grid[i][j - 1] + grid[i][j];
-                } else if (j == 0) {
-                    grid[i][j] = grid[i - 1][j] + grid[i][j];
+                    // grid[i][j] = grid[i][j - 1] + grid[i][j];
+                    dp[i][j] = grid[i][j - 1] + grid[i][j];
+                } else if (j == 0) { // left border
+                    // grid[i][j] = grid[i - 1][j] + grid[i][j];
+                    dp[i][j] = grid[i - 1][j] + grid[i][j];
                 } else {
-                    grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
-                }  
+                    // grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+                }
             }
         }
-        return grid[row - 1][col - 1];
+        // return grid[row - 1][col - 1];
+        return dp[row - 1][col - 1];
     }
 }
 
